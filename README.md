83ab913bbc6d058a7fd66abdb06753ef29366148
shota-ootani-excite <shota.ootani@excite-software.co.jp>
2024-01-16 16:36:57 +0900

    API一式を作成

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # 構築メモ
 1. OracleJDKをインストールした。
 2. mavenをインストールした。
+
+# VSCodeの拡張機能
+- Extension Pack for Java
+- Spring Boot Dashboard
+- REST Client
--- src/main/java/jp/co/software/excite/training/application/controller/TaskController.java
+++ src/main/java/jp/co/software/excite/training/application/controller/TaskController.java
@@ -3,7 +3,10 @@ package jp.co.software.excite.training.application.controller;
 import java.util.List;
 import java.util.Objects;
 import org.springframework.web.bind.annotation.RestController;
+import jp.co.software.excite.training.application.resource.TaskPostRequest;
+import jp.co.software.excite.training.application.resource.TaskPutRequest;
 import jp.co.software.excite.training.application.resource.TaskResponse;
+import jp.co.software.excite.training.domain.dto.TaskDto;
 import jp.co.software.excite.training.domain.service.TaskService;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -11,8 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -46,5 +53,37 @@ public class TaskController {
 
         var resp = modelMapper.map(model, TaskResponse.class);
         return new ResponseEntity<>(resp, HttpStatus.OK);
+    }
+
+    @PostMapping("/tasks")
+    public ResponseEntity<TaskResponse> postTask(@RequestBody TaskPostRequest req) {
+
+        var reqData = modelMapper.map(req, TaskDto.class);
+
+        var model = service.createTask(reqData);
+
+        var resp = modelMapper.map(model, TaskResponse.class);
+        return new ResponseEntity<>(resp, HttpStatus.OK);
+    }
+
+    @PutMapping("/tasks/{id}")
+    public ResponseEntity<TaskResponse> putTask(@PathVariable Integer id,
+            @RequestBody TaskPutRequest req) {
+
+        var reqData = modelMapper.map(req, TaskDto.class);
+        reqData.setId(id);
+
+        var model = service.updateTask(reqData);
+
+        var resp = modelMapper.map(model, TaskResponse.class);
+        return new ResponseEntity<>(resp, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Integer id) {
+
+        service.deleteTask(id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
